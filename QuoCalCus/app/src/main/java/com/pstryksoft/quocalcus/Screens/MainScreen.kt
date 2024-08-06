c9fa5c8b93c344893a02f4eb68498b0ec2450945
pstryk1 <150193642+pstryk1@users.noreply.github.com>
2024-08-06 20:09:34 +0200

    Naprawa

--- QuoCalCus/app/src/main/java/com/pstryksoft/quocalcus/Screens/MainScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.pstryksoft.quocalcus.Screens
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-@Composable
-fun MainScreen(modifier: Modifier = Modifier) {
-    
-}
